65d8cb8b67990bc8a22e90d04efcd4e9f78bf348
riponmondalbd <riponmondal149@gmail.com>
2024-07-05 22:15:16 +0600

    toggle state based on click and conditional randaring

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
+
 const Countries = () => {
   const [countries, setCountries] = useState([]);
 
@@ -13,9 +15,11 @@ const Countries = () => {
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
-      {countries.map((country) => (
-        <Country key={country.cca3} country={country}></Country>
-      ))}
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
