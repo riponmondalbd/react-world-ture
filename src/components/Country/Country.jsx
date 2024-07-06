ab2605f6cd87442cce7c9412cf32d7d27f448226
riponmondalbd <riponmondal149@gmail.com>
2024-07-06 22:03:39 +0600

    toggle state based on click and conditional rendering

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags, area, population } = country;
+  const { name, flags, area, population, cca3 } = country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
   return (
     <div className="country">
-      <h3>Name: {name.common}</h3>
+      <h3>{name.common}</h3>
       <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
+      <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+      {visited ? "I have Visited this Country." : "I want to Visit"}
     </div>
   );
 };
